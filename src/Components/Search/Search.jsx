b457bae4fb341f238d5a6fe61402d3d80263c961
Gayal Melappilly <gayalsunil@gmail.com>
2024-03-20 00:00:27 +0530

    Search feature added

--- src/Components/Search/Search.jsx
+++ src/Components/Search/Search.jsx
@@ -1,12 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Search.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCircleLeft, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from 'react-router-dom'
+import { API_KEY } from '../../Constants/constants'
+import axios from '../../axios'
+import RowPost from '../RowPost/RowPost'
 
 function Search() {
+  const navigate = useNavigate()
+  const [movie, setMovie] = useState('')
+  // const [displayMv, setDisplayMv] = useState('')
+  useEffect(() => {
+    // alert(`movie/${id}??api_key=${API_KEY}&language=en-US`)
+    axios.get(`search/movie?api_key=${API_KEY}&query=${movie}&include_adult=false&language=en-US&page=1`).then((response) => {
+        // console.log(response.data)
+        console.log(response.data)
+    })
+}, [movie])
   return (
-    <div>
-      <h1 className='title'>Browse</h1>
+    <div className='search'>
+      <FontAwesomeIcon className='back-icon' icon={faCircleLeft} onClick={()=>navigate(-1)} />
+      <h1 className='title' id='browse-title'>Browse</h1>
       <div className="search-div">
-        <input type="text" className='search' />
+        <input type="text" id='search-input' className='search-input' onChange={(e)=>setMovie(e.target.value)} />
+        <FontAwesomeIcon className='search-icon' icon={faMagnifyingGlass} />
+        {console.log("SEARCH : "+movie)}
+      </div>
+      <div className='display'>
+        {movie.total_results === 0 ? <RowPost title="Not Found" /> : <RowPost title="Results" url={`search/movie?api_key=${API_KEY}&query=${movie}&include_adult=false&language=en-US&page=1`} isSmall />}
       </div>
     </div>
   )
